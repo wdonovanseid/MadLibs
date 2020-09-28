ee1c39c60768c118b90b08df770eccb5bfc1af2d
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-28 13:26:56 -0700

    Added css and linked it to our homepage.
    Co-authored by: Will DS <wdstwo@live.com>

--- MadLibs/Startup.cs
+++ MadLibs/Startup.cs
@@ -26,6 +26,7 @@ namespace MadLibs
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
+      app.UseStaticFiles();
 
       app.UseMvc(routes =>
       {
